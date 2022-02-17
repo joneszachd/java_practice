d4a50592e5cf6723df96c6f2ba41292f9f32ea31
Zach <joneszachd@gmail.com>
2022-02-17 00:21:42 -0600

    Ch-ch-ch-ch-changes!

---
+++ operation.java
@@ -0,0 +1,22 @@
+// Math quiz!
+
+class operation {
+	public static void main(String[] args) {
+// What's 3 + 3?
+	int add = 3 + 3;
+	System.out.println(add);
+// 3 - 3?
+	int sub = 3 - 3;
+	System.out.println(sub);
+// 3 * 3
+	int mul = 3 * 3;
+	System.out.println(mul);
+// Hey! Programming's cheating! Now divide!
+	int div = 3 / 3;
+	System.out.println(div);
+
+// Modulus will return the remainer
+	int mod = 4 % 3;
+	System.out.println(mod);
+	}
+}
--- types.java
+++ types.java
@@ -20,5 +20,17 @@ class types {
 //... Okay, that's not all, Arrays, Classes, and Interfaces are all non-primitive as well. This is because they all technically refer to objects. A String is simply a collection of char 
 
 		System.out.println(Num);
+
+// Casting
+
+// Converting our Integer to a larger Data Type
+double trouble = Num;
+System.out.println(trouble);
+
+long boy = Num;
+System.out.println(boy);
+
+//short man = Num; can't happen, this is a conversion from a 4 byte int to a 2 byte short
+//System.out.println(man);
 	}
 }

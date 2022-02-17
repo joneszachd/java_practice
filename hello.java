77a2a65e220b4152a9b32c54a48ea636ae029e89
Zach <joneszachd@gmail.com>
2022-02-16 23:26:06 -0600

    First examples

---
+++ hello.java
@@ -0,0 +1,7 @@
+// A basic hello world program in Java!
+
+class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ types.java
@@ -0,0 +1,24 @@
+// A basic programming introducing various data types in Java
+
+// Primitive Data Type Variables
+class types {
+	public static void main(String[] args) {
+// Integer
+		int Num = 7;
+// Float
+		float floatNum = 3.14f;
+// Character
+		char theAlpha = 'r';
+// Boolean
+		boolean yes = true;
+
+// Non-Primitive/Reference Data Types... 
+
+//String
+		String hello = "Hey hey hey!";
+
+//... Okay, that's not all, Arrays, Classes, and Interfaces are all non-primitive as well. This is because they all technically refer to objects. A String is simply a collection of char 
+
+		System.out.println(Num);
+	}
+}
